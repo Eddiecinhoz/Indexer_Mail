9cb770ab04a6f81fd2a04af3c176ecd531a4e55f
Eddiegomaz <eddie.gonzalez@peaku.ai>
2024-01-31 22:19:19 -0500

    Creating Repository

---
+++ Frontend/email-front/src/components/EmailView.vue
@@ -0,0 +1,170 @@
+<template>
+    <div class="mx-w-screen-lg p-5">
+        <div class="flex justify-center w-full py-2">
+            <input v-model="filterConfig.text" class="w-10/12 border rounded-l-lg" placeholder="Search" @keydown="getEmails()">
+            <button class=" bg-indigo-500 py-2 px-10 text-white rounded-r-lg" @click="getEmails()">
+                <v-icon name="fa-search"></v-icon>
+            </button>
+        </div>
+        <div class="grid grid-cols-2 pt-6">
+            <div class="w-11/12 mx-auto">
+                <div class="rounded-md border-2 border-indigo-500 overflow-x-auto " style="max-height: 70vh; height: 70vh;">
+                    <table class=" border-separate w-full">
+                        <thead class="sticky top-0">
+                            <tr class="bg-indigo-400 text-white">
+                                <th class="cursor-pointer w-1/4 max-w-1/4 py-2" v-for="column in tableColumns" @click="sortColumn(column)">
+                                    {{ column }}
+                                    <v-icon v-if="column.toLowerCase() == keywordSort" name="fa-sort-down"></v-icon>
+                                    <v-icon v-else-if="'-'+column.toLowerCase() == keywordSort" name="fa-sort-up"></v-icon>
+                                    <v-icon v-else name="fa-sort"></v-icon>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody v-if="!loadingMails">
+                            <tr class="cursor-pointer hover:bg-gray-300" :class="highLightRowClass(email)" v-for="email in emails" @click="setEmail(email)">
+                                <td class="max-w-0 truncate">{{ email.from }}</td>
+                                <td class="max-w-0 truncate">{{ email.to }}</td>
+                                <td class="max-w-0 truncate">{{ email.subject }}</td>
+                                <td class="max-w-0 truncate">{{ email.directory }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <div class="flex justify-center size-full text-center" v-if="loadingMails">
+                        <h4 class="my-auto ">
+                            <v-icon class="text-indigo-500" name="fa-spinner" scale="2" animation="spin-pulse"></v-icon>
+                            Loading...
+                        </h4>
+                    </div>
+                </div>
+                <div class="flex justify-between text-indigo-500 pt-1" v-if="!loadingMails">
+                    <v-icon name="fa-arrow-alt-circle-left" scale="1.5" class="cursor-pointer" @click="changePage('back')"></v-icon>
+                    {{ currentPage }}  / {{ Math.ceil(totalEmails/40) }}
+                    <v-icon name="fa-arrow-alt-circle-right" scale="1.5" class="cursor-pointer" @click="changePage('next')"></v-icon>
+                </div>
+            </div>
+            <div class="flex flex-col justify-left my-auto size-full">
+                <template v-if="selectedEmail">
+                    <h2 class="text-xl font-bold ">{{ selectedEmail.subject }}</h2>
+                    <div class="rounded-lg border-2 border-indigo-500 w-11/12 h-5/6 my-auto p-6 h-f" style="max-height: 50vh; overflow-x: hidden;">
+                        <p><b>From:</b> {{ selectedEmail.from }} </p>
+                        <p><b>To:</b> {{ selectedEmail.to }} </p>
+                        <br>
+                        <p><b>Subject:</b> {{ selectedEmail.subject }} </p>
+                        <br>
+                        {{ selectedEmail.content }}
+                        <br>
+                        <br>
+                        <p><b>Locate:</b> {{ selectedEmail.directory }} </p>
+                    </div>
+                </template>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import axios from "../utils/axios"
+import { onMounted } from 'vue';
+import Swal from 'sweetalert2';
+
+const selectedEmail = ref(null)
+let emails = ref([]);
+let totalEmails = ref(0)
+let currentPage = ref(1)
+let loadingMails = ref(false)
+let tableColumns = ['From', 'To', 'Subject', 'Directory']
+let keywordSort = ref("_score")
+let filterConfig = ref({
+    text: null,
+    column: null,
+})
+
+function sortColumn(column){
+    let keyword = column.toLowerCase()
+    if ('-'+keyword == keywordSort.value){
+        keywordSort.value='_score'
+    } else if (keyword == keywordSort.value){
+        keywordSort.value='-'+keyword
+    } else {
+        keywordSort.value=keyword
+    }
+    getEmails()
+}
+
+/*Methods*/
+function setEmail(email){
+    selectedEmail.value=email
+}
+function highLightRowClass(email){
+    return selectedEmail.value?.id == email.id ? 'bg-gray-400' : ''
+}
+
+function changePage(source){
+    console.log(totalEmails.value/40)
+    if (source == 'next'){
+        currentPage.value++
+        if ( currentPage.value > Math.ceil(totalEmails.value/40) ){
+            Swal.fire({
+                icon: "warning",
+                text: "No more pages"
+            })
+            currentPage.value--
+        }
+    } else if (source == 'back'){
+        currentPage.value--
+        if ( currentPage.value <= 0 ){
+            Swal.fire({
+                icon: "warning",
+                title: "No more pages"
+            })
+            currentPage.value ++
+        }
+    }
+    getEmails('pagination')
+}
+
+async function getEmails(source='filter'){
+    loadingMails.value=true
+    let textQuery=null
+    if (source == "filter"){
+        currentPage.value = 1
+    }
+    if (filterConfig.value?.text){
+        textQuery = (filterConfig.value.text).trim().replaceAll(' ','+')
+    }
+    if (keywordSort.value != '_score'){
+        let columnToFilter = keywordSort.value.replaceAll("-","")
+        textQuery = textQuery ? columnToFilter+":"+textQuery : null
+    }
+    let params = {
+        text: textQuery,
+        start: (currentPage.value - 1) * 40,
+        step: 40,
+        order_by: keywordSort.value
+    }
+    let res = await axios.post("/get_emails", params, ); 
+    emails.value = []    
+    emails.value = res.data.Hits.map((hit)=>{
+        return {
+            id: hit._source._id,
+            directory: hit._source.Directory,
+            content: hit._source.Content,
+            from: hit._source.From,
+            to: hit._source.To,
+            subject: hit._source.Subject
+        }    
+    })
+    totalEmails.value = res.data.Total.Value
+    selectedEmail.value = emails.value[0]
+    loadingMails.value=false
+}
+
+onMounted(() => {
+    getEmails()
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ Frontend/email-front/src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <nav class="bg-indigo-500 relative">
+        <div class="mx-auto flex justify-center">
+            <div class="flex w-1/5 justify-center text-white font-bold text-xl">
+                <a href="#" class="p-5 hover:bg-white hover:text-indigo-500 ">Home</a>
+                <a href="#" class="p-5 hover:bg-white hover:text-indigo-500">Emails</a>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
